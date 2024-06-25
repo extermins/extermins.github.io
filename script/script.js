865d8494154768adfccc71329158df299bc7569d
extermins <abab1070@naver.com>
2024-06-26 00:26:24 +0900

    디자인 수정

--- script/script.js
+++ script/script.js
@@ -25,3 +25,22 @@ document.addEventListener('DOMContentLoaded', function() {
   });
 });
 
+
+let slideIndex = 0;
+
+    function showSlide(index) {
+        const slides = document.querySelector('.slides');
+        const totalSlides = document.querySelectorAll('.slide').length;
+
+        if (index >= totalSlides) { slideIndex = 0; }
+        if (index < 0) { slideIndex = totalSlides - 1; }
+
+        slides.style.transform = 'translateX(' + (-slideIndex * 100) + '%)';
+    }
+
+    function moveSlide(n) {
+        showSlide(slideIndex += n);
+    }
+
+    showSlide(slideIndex);
+
